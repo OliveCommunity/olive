f82c69bb5ece2fe57d49304ab5b645d375b57748
AptGetMe <awford@fastmail.com>
2026-02-19 17:09:31 -0600

    Finalize copyright header clearifications - app/ts - docs/.vuepress/config.ts - ext/CMakeList.txt - ext/core - third_party/openfx/include - third_party/openfx/logos/svg - third_party/openfx/Support/include/osxDeploy.sh

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,3 +1,11 @@
+/*
+  This file is part of Oak Video Editor - A fork of original project Olive 
+
+  SPDX-License-Identifier: GPL-3.0-only
+  Copyright (C) 2025 mikesolar
+
+*/
+
 import { defineUserConfig } from "vuepress";
 import { hopeTheme } from "vuepress-theme-hope";
 
